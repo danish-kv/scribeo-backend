31542b361e10e151103c33ad5d891afa01f4c4e9
Danish <muhammeddanishkv05@gmail.com>
2024-11-03 00:16:45 +0530

    feat:(Blog):- added blog models and views, urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog.views import PostViewSet
+from users.views import CustomTokenObtainPairView, RegisterAPIView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(f"posts", PostViewSet, basename='posts')
 
 urlpatterns = [
+    path("", include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('api/register/', RegisterAPIView.as_view(), name='register')
+    
 ]
---
+++ backend/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    
