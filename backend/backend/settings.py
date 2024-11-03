9afa3137dcab848d98181dd53d101498851b2cfc
Danish <muhammeddanishkv05@gmail.com>
2024-11-03 17:42:41 +0530

    feat: customize jwt token, added logout api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'users',
     'blog',
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
@@ -8,6 +9,7 @@ from rest_framework.decorators import api_view
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.exceptions import TokenError
 
 from .serializers import UserSerializer, RegisterSerializer
 
@@ -36,5 +38,41 @@ class RegisterAPIView(APIView):
 class CustomTokenObtainPairView(TokenObtainPairView):
     permission_classes = [AllowAny]
 
+    def post(self, request: Request, *args, **kwargs) -> Response:
+
+        try:
+            res =  super().post(request, *args, **kwargs)
+            print(res)
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            user = serializer.user
+            res.data['user'] = user.username
+            res.data['userID'] = user.id
+            print('last res ====', res)
+            return res
+        except Exception as e:
+            print('exeption', e)
+            return Response(data=str(e), status=status.HTTP_404_NOT_FOUND)
+        
 
 
+
+class Logout(APIView):
+    def post(self, request):
+        try:
+            refresh = request.data.get('refresh')
+            if not refresh:
+                return Response({"detail": "Refresh token is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+            token = RefreshToken(refresh)
+            token.blacklist()
+
+            return Response({"detail": "Successfully logged out."}, status=status.HTTP_205_RESET_CONTENT)
+
+        except TokenError as e:
+            return Response({"detail": "Invalid or expired token."}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            # General error handling
+            print(e)
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
