f9faf732e694fcf7e6f03293e1bbf857157acdaa
Danish <muhammeddanishkv05@gmail.com>
2024-11-12 10:16:21 +0530

    added static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,6 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
 
 
 print(cloudinary.config().cloud_name)
